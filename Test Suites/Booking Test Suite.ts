6135d063334b0eeeb73fb2f1455736bc06992a5b
Henry <taint.1702@gmail.com>
2024-05-22 23:58:48 +0700

    add more test cases

---
+++ Test Suites/Booking Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Booking Test Suite</description>
+   <name>Booking Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2f43f191-cd38-4dfd-90df-1234e949abed</testSuiteGuid>
+   <testCaseLink>
+      <guid>23288742-0ca9-4544-ae1c-21bf82c630ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1_Booking_Successfully</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
